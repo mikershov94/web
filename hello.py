1a6cfced257aec7a6e13010cee600d5e582489ed
mikershov94 <ershovme@gmail.com>
2018-03-29 13:54:07 +0800

    Config and start gunicorn

---
+++ hello.py
@@ -0,0 +1,15 @@
+def wsgi_app(environ, start_response):
+	query = environ['QUERY_STRING'].split('&')
+	for i in range(len(query)):
+		query[i] += '\n'
+	body = [bytes(i, 'ascii') for i in query]
+	status = '200 OK'
+	headers = [
+		('Content-Type', 'text/plain')
+	]
+	start_response(status, headers)
+	return body
+
+	#Gunicorn config
+	bind = '0.0.0.0:8000'
+		
--- init.sh
+++ init.sh
@@ -2,8 +2,8 @@ sudo mkdir /home/box/logs
 sudo touch /home/box/logs/errors.log
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-#sudo ln -s /home/box/web/hello.py       /etc/gunicorn.d/hello.py
-#sudo /etc/init.d/gunicorn restart
+sudo ln -s /home/box/web/hello.py       /etc/gunicorn.d/hello.py
+sudo /etc/init.d/gunicorn restart
 #sudo /etc/init.d/mysql start
 #sudo mysql -uroot -e "create datbase db_ask"
 git config --global user.email "ershovme@gmail.com"
