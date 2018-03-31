194a33b6ec59a13d238897924df5ebd1442f0634
mikershov94 <ershovme@gmail.com>
2018-03-31 16:20:40 +0800

    Fixed error3 with urls

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls import url
 from qa.views import test
 
 urlpatterns = [
-	url(r'^', test),
-	url(r'^login/', test),
-	url(r'^signup/', test),
-	url(r'^question/(\d+)/', test),
-	url(r'^ask/', test),
-	url(r'^popular/', test),
-	url(r'^new/', test),
+	url(r'^$', test),
+	url(r'^login/$', test),
+	url(r'^signup/$', test),
+	url(r'^question/(\d+)/$', test),
+	url(r'^ask/$', test),
+	url(r'^popular/$', test),
+	url(r'^new/$', test),
 ]
