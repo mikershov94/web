1801594df75df200e511a85978b12fde26632278
mikershov94 <ershovme@gmail.com>
2018-03-31 16:00:03 +0800

    Fixed error2 wsgi

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ sudo ln -s /home/box/web/hello.py       /etc/gunicorn.d/hello.py
 sudo ln -s /home/box/web/etc/ask_gunicorn_conf.py /etc/gunicorn.d/ask_gunicorn_conf.py
 sudo /etc/init.d/nginx start
 sudo gunicorn -c hello.py hello:wsgi_app &
-sudo gunicorn -c ask_gunicorn_conf.py ask.wsgi:aplication &
+sudo gunicorn -c ask_gunicorn_conf.py ask.wsgi:application &
 #sudo /etc/init.d/mysql start
 #sudo mysql -uroot -e "create datbase db_ask"
 git config --global user.email "ershovme@gmail.com"
