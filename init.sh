0ce492ed430c45b078c04fb99722ca1b950b49a7
mikershov94 <ershovme@gmail.com>
2018-03-31 23:22:06 +0800

    Added start MySQL in init.sh

--- init.sh
+++ init.sh
@@ -7,7 +7,7 @@ sudo ln -s /home/box/web/etc/ask_gunicorn_conf.py /etc/gunicorn.d/ask_gunicorn_c
 sudo /etc/init.d/nginx start
 sudo gunicorn -c hello.py hello:wsgi_app &
 sudo gunicorn -c /home/box/web/etc/ask_gunicorn_conf.py ask.wsgi:application &
-#sudo /etc/init.d/mysql start
-#sudo mysql -uroot -e "create datbase db_ask"
+sudo /etc/init.d/mysql start
+sudo mysql -u root -e "create database db_ask"
 git config --global user.email "ershovme@gmail.com"
 git config --global user.name "mikershov94"
