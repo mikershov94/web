4320d0ae08f95b02575b04ced601298beb53487e
mikershov94 <ershovme@gmail.com>
2018-03-31 23:43:15 +0800

    Added models QUESTION and ANSWER in models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,5 +1,19 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Question(models.Model):
+	title = models.CharField(max_length=255)
+	text = models.TextField()
+	added_at = models.DateTimeField()
+	rating = models.IntegerField()
+	author = models.ForeignKey(User)
+	likes = models.ManyToManyField(User, related_name='likes_set')
+
+class Answer(models.Model):
+	text = models.TextField()
+	added_at = models.DateTimeField()
+	qustion = models.ForeignKey(Question, null=True, on_delete=models.CASCADE)
+	author = models.ForeignKey(User)	
