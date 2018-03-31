e04b92251ba224d0db4855f843f5b2cfede4f6e0
mikershov94 <ershovme@gmail.com>
2018-03-31 15:36:08 +0800

    Edit init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 sudo mkdir /home/box/logs
 sudo touch /home/box/logs/errors.log
+sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/hello.py       /etc/gunicorn.d/hello.py
