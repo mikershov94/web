29fc70ba0c7c52b174c11a823e34ffb94706599d
mikershov94 <ershovme@gmail.com>
2018-03-31 15:52:02 +0800

    Edit wsgi conf

--- init.sh
+++ init.sh
@@ -2,9 +2,11 @@ sudo mkdir /home/box/logs
 sudo touch /home/box/logs/errors.log
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/hello.py       /etc/gunicorn.d/hello.py
-sudo /etc/init.d/gunicorn restart
+sudo ln -s /home/box/web/etc/ask_gunicorn_conf.py /etc/gunicorn.d/ask_gunicorn_conf.py
+sudo /etc/init.d/nginx start
+sudo gunicorn -c hello.py hello:wsgi_app &
+sudo gunicorn -c ask_gunicorn_conf ask.wsgi:aplication &
 #sudo /etc/init.d/mysql start
 #sudo mysql -uroot -e "create datbase db_ask"
 git config --global user.email "ershovme@gmail.com"
