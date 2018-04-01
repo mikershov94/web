fabb6a729edff42d20c4a7a521744cd76a7dc47f
mikershov94 <ershovme@gmail.com>
2018-04-01 16:44:59 +0800

    Fixed SQL command

--- init.sh
+++ init.sh
@@ -9,8 +9,9 @@ sudo gunicorn -c hello.py hello:wsgi_app &
 sudo gunicorn -c /home/box/web/etc/ask_gunicorn_conf.py ask.wsgi:application &
 sudo /etc/init.d/mysql start
 sudo mysql -u root -e "CREATE DATABASE db_ask"
-sudo mysql -u root -e "CREATE USER ask_user@localhost"
+sudo mysql -u root -e "CREATE USER ask_user@localhost IDENTIFIED BY '12345678'"
 sudo mysql -u root -e "GRANT ALL PRIVILEGES ON db_ask.* TO ask_user@localhost"
+sudo mysql -u root -e "FLUSH PRIVILEGES"
 sudo python /home/box/web/ask/manage.py makemigrations qa #заменить на migrate!!!!!!!
 git config --global user.email "ershovme@gmail.com"
 git config --global user.name "mikershov94"
