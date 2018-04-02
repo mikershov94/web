2913b61e9d3bac109337ec3603dfa60c32131033
mikershov94 <ershovme@gmail.com>
2018-04-02 13:28:21 +0800

    Fixed errors

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -13,15 +13,15 @@ class QuestionManager(models.Manager):
 
 class Question(models.Model):
 	objects = QuestionManager()
-	title = models.CharField(max_length=255)
+	title = models.CharField(max_length=50)
 	text = models.TextField()
-	added_at = models.DateTimeField()
+	added_at = models.DateTimeField(auto_now_add=True)
 	rating = models.IntegerField(default=0)
 	author = models.ForeignKey(User)
 	likes = models.ManyToManyField(User, related_name='likes_set')
 
 class Answer(models.Model):
 	text = models.TextField()
-	added_at = models.DateTimeField()
-	question = models.ForeignKey(Question, null=True, on_delete=models.CASCADE)
+	added_at = models.DateTimeField(auto_now_add=True)
+	question = models.ForeignKey(Question)
 	author = models.ForeignKey(User)	
