443af6cefdcf7a35b2ee5127e9dead42500dcd63
mikershov94 <ershovme@gmail.com>
2018-04-11 15:38:16 +0800

    In views import libs

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from django.core.paginator import Paginator
+from qa.models import Question, Answer
 
 # Create your views here.
 def test(request, *args, **kwargs):
