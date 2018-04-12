2e8c9ca41c27d60cf7d5cbba18cc19edd6bb9282
mikershov94 <ershovme@gmail.com>
2018-04-12 14:43:41 +0800

    In urls.py edited routes

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls import url
 from qa.views import test
 
 urlpatterns = [
-	url(r'^$', test),
+	url(r'^$', question_list_new),
 	url(r'^login/$', test),
 	url(r'^signup/$', test),
-	url(r'^question/(\d+)/$', test),
+	url(r'^question/(\d+)/$', question_details),
 	url(r'^ask/$', test),
-	url(r'^popular/$', test),
+	url(r'^popular/$', question_popular),
 	url(r'^new/$', test),
 ]
