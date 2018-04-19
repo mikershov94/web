b753c90a6dd626c9b94a3f660bf461ba6fd9a1fc
mikershov94 <ershovme@gmail.com>
2018-04-19 15:28:39 +0800

    Fixed urls error

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import url
-from qa.views import *
 
 urlpatterns = [
-	url(r'^$', question_list_new),
+	url(r'^$', 'qa.views.question_list_new', name='question_list_new'),
 	url(r'^login/$', test),
 	url(r'^signup/$', test),
-	url(r'^question/(\d+)/$', question_details),
+	url(r'^question/(\d+)/$', 'qa.views.question_details', name='question_details'),
 	url(r'^ask/$', test),
-	url(r'^popular/$', question_popular),
+	url(r'^popular/$', 'qa.views.question_popular', name='question_popular'),
 	url(r'^new/$', test),
 ]
