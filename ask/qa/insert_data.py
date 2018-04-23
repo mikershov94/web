e7bdc9d548cb9e41b1690fb75b97d198b09147a6
mikershov94 <ershovme@gmail.com>
2018-04-23 11:16:08 +0800

    Edit insert_data.py

--- ask/qa/insert_data.py
+++ ask/qa/insert_data.py
@@ -14,116 +14,116 @@ q1 = Question.objects.create(
 		author = u1,
 	)
 q2 = Question.objects.create(
-		title = 'Computer',
-		text = 'How connect Internet?',
+		title = 'TV',
+		text = 'How move TV?',
 		rating = 3,
 		author = u1,
 	)
 q3 = Question.objects.create(
-		title = 'Computer',
-		text = 'How connect Internet?',
+		title = 'Sun',
+		text = 'Where wake up sun?',
 		rating = 3,
 		author = u1,
 	)
 q4 = Question.objects.create(
-		title = 'Computer',
-		text = 'How connect Internet?',
+		title = 'Water',
+		text = 'How clean water?',
 		rating = 3,
 		author = u1,
 	)
 q5 = Question.objects.create(
-		title = 'Computer',
-		text = 'How connect Internet?',
+		title = 'Clip',
+		text = 'How create clip?',
 		rating = 3,
 		author = u1,
 	)
 q6 = Question.objects.create(
-		title = 'Computer',
-		text = 'How connect Internet?',
+		title = 'YouTube',
+		text = 'How log up to YouTube?',
 		rating = 3,
 		author = u1,
 	)
 q7 = Question.objects.create(
-		title = 'Computer',
-		text = 'How connect Internet?',
+		title = 'Desert',
+		text = 'Do you want desert?',
 		rating = 3,
 		author = u1,
 	)
 q8 = Question.objects.create(
-		title = 'Computer',
-		text = 'How connect Internet?',
+		title = 'Video',
+		text = 'How record video?',
 		rating = 3,
 		author = u1,
 	)
 q9 = Question.objects.create(
-		title = 'Computer',
-		text = 'How connect Internet?',
+		title = 'Club',
+		text = 'How input to club?',
 		rating = 3,
 		author = u1,
 	)
 q10 = Question.objects.create(
-		title = 'Computer',
-		text = 'How connect Internet?',
+		title = 'Sound',
+		text = 'How create cool sound?',
 		rating = 3,
 		author = u1,
 	)
 q11 = Question.objects.create(
-		title = 'Computer',
-		text = 'How connect Internet?',
+		title = 'Games',
+		text = 'Do you like videogames?',
 		rating = 3,
 		author = u1,
 	)
 q12 = Question.objects.create(
-		title = 'Computer',
-		text = 'How connect Internet?',
+		title = 'Films',
+		text = 'Do you like watch films?',
 		rating = 3,
 		author = u1,
 	)
 q13 = Question.objects.create(
-		title = 'Computer',
-		text = 'How connect Internet?',
+		title = 'What films',
+		text = 'What films you like??',
 		rating = 3,
 		author = u1,
 	)
 q14 = Question.objects.create(
-		title = 'Computer',
-		text = 'How connect Internet?',
+		title = 'Guitar',
+		text = 'How play on guitar?',
 		rating = 3,
 		author = u1,
 	)
 q15 = Question.objects.create(
-		title = 'Computer',
-		text = 'How connect Internet?',
+		title = 'Accords',
+		text = 'Accords - what is it?',
 		rating = 3,
 		author = u1,
 	)
 q16 = Question.objects.create(
-		title = 'Computer',
-		text = 'How connect Internet?',
+		title = 'MIDI',
+		text = 'How connect midi-keyboard to computer?',
 		rating = 3,
 		author = u1,
 	)
 q17 = Question.objects.create(
-		title = 'Computer',
-		text = 'How connect Internet?',
+		title = 'Nadya Dorofeeva',
+		text = 'Do you like Nadya Dorofeeva?',
 		rating = 3,
 		author = u1,
 	)
 q18 = Question.objects.create(
-		title = 'Computer',
-		text = 'How connect Internet?',
+		title = 'Programmist',
+		text = 'You programmist?',
 		rating = 3,
 		author = u1,
 	)
 q19 = Question.objects.create(
-		title = 'Computer',
-		text = 'How connect Internet?',
+		title = 'Your names',
+		text = 'What is your names?',
 		rating = 3,
 		author = u1,
 	)
 q20 = Question.objects.create(
-		title = 'Computer',
-		text = 'How connect Internet?',
+		title = 'Tickets',
+		text = 'Where buy tickets to concert?',
 		rating = 3,
 		author = u1,
 	)
