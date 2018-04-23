a41a80a547a5039f9f7bf601d42b73a4d67f2d2d
mikershov94 <ershovme@gmail.com>
2018-04-23 11:03:37 +0800

    Edit insert_data.py

--- ask/qa/insert_data.py
+++ ask/qa/insert_data.py
@@ -8,26 +8,122 @@ u4 = UserManager.create_user(Helen, email = 'helen@qa.com', password = '')
 u5 = UserManager.create_user(Patrick, email = 'pat@qa.com', password = '')
 
 q1 = Question.objects.create(
-		title = ,
-		text = ,
-
+		title = 'Computer',
+		text = 'How connect Internet?',
+		rating = 3,
+		author = u1,
 	)
-q2
-q3
-q4
-q5
-q6
-q7
-q8
-q9
-q10
-q11
-q12
-q13
-q14
-q15
-q16
-q17
-q18
-q19
-q20
+q2 = Question.objects.create(
+		title = 'Computer',
+		text = 'How connect Internet?',
+		rating = 3,
+		author = u1,
+	)
+q3 = Question.objects.create(
+		title = 'Computer',
+		text = 'How connect Internet?',
+		rating = 3,
+		author = u1,
+	)
+q4 = Question.objects.create(
+		title = 'Computer',
+		text = 'How connect Internet?',
+		rating = 3,
+		author = u1,
+	)
+q5 = Question.objects.create(
+		title = 'Computer',
+		text = 'How connect Internet?',
+		rating = 3,
+		author = u1,
+	)
+q6 = Question.objects.create(
+		title = 'Computer',
+		text = 'How connect Internet?',
+		rating = 3,
+		author = u1,
+	)
+q7 = Question.objects.create(
+		title = 'Computer',
+		text = 'How connect Internet?',
+		rating = 3,
+		author = u1,
+	)
+q8 = Question.objects.create(
+		title = 'Computer',
+		text = 'How connect Internet?',
+		rating = 3,
+		author = u1,
+	)
+q9 = Question.objects.create(
+		title = 'Computer',
+		text = 'How connect Internet?',
+		rating = 3,
+		author = u1,
+	)
+q10 = Question.objects.create(
+		title = 'Computer',
+		text = 'How connect Internet?',
+		rating = 3,
+		author = u1,
+	)
+q11 = Question.objects.create(
+		title = 'Computer',
+		text = 'How connect Internet?',
+		rating = 3,
+		author = u1,
+	)
+q12 = Question.objects.create(
+		title = 'Computer',
+		text = 'How connect Internet?',
+		rating = 3,
+		author = u1,
+	)
+q13 = Question.objects.create(
+		title = 'Computer',
+		text = 'How connect Internet?',
+		rating = 3,
+		author = u1,
+	)
+q14 = Question.objects.create(
+		title = 'Computer',
+		text = 'How connect Internet?',
+		rating = 3,
+		author = u1,
+	)
+q15 = Question.objects.create(
+		title = 'Computer',
+		text = 'How connect Internet?',
+		rating = 3,
+		author = u1,
+	)
+q16 = Question.objects.create(
+		title = 'Computer',
+		text = 'How connect Internet?',
+		rating = 3,
+		author = u1,
+	)
+q17 = Question.objects.create(
+		title = 'Computer',
+		text = 'How connect Internet?',
+		rating = 3,
+		author = u1,
+	)
+q18 = Question.objects.create(
+		title = 'Computer',
+		text = 'How connect Internet?',
+		rating = 3,
+		author = u1,
+	)
+q19 = Question.objects.create(
+		title = 'Computer',
+		text = 'How connect Internet?',
+		rating = 3,
+		author = u1,
+	)
+q20 = Question.objects.create(
+		title = 'Computer',
+		text = 'How connect Internet?',
+		rating = 3,
+		author = u1,
+	)
