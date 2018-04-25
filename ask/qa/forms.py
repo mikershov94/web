5c9a3a69bd070e123a02148c64d997f7d93d4f8c
mikershov94 <ershovme@gmail.com>
2018-04-25 14:15:26 +0800

    Edit forms.py

--- ask/qa/forms.py
+++ ask/qa/forms.py
@@ -3,9 +3,17 @@ from django import forms
 class AskForm(forms.ModelForm):
 	class Meta:
 		model = Question
-		fields = ['title', 'text']
+		fields = ['title']
+	text = forms.CharField(widget=forms.TextArea)
+
+	def clean(self):
+		if is_spam(self.cleaned_data):
+			raise forms.ValidationError(u'Сообщение похоже на спам', code='spam') 
 
 class AnswerForm(forms.ModelForm):
-    class Meta:
-        model = Answer
-        fields = ['text', 'question']
+    text = forms.CharField(widget=forms.TextArea)
+    question = forms.CharField()
+
+    def clean(self):
+		if is_spam(self.cleaned_data):
+			raise forms.ValidationError(u'Сообщение похоже на спам', code='spam') 
