205a23ac29ceab8ff8bd9ad13f40839eaf90fff3
mikershov94 <ershovme@gmail.com>
2018-04-26 11:08:02 +0800

    Fixed error

--- ask/qa/forms.py
+++ ask/qa/forms.py
@@ -1,14 +1,28 @@
 from django import forms
 from qa.models import Question, Answer
 
+def is_ask(cleaned_data):
+	text = cleaned_data['text']
+	if text[len(text)] == '?':
+		return True
+	else:
+		return False
+
+def is_answer(cleaned_data):
+	text = cleaned_data['text']
+	if text[len(text)] != '?':
+		return True
+	else:
+		return False
+
 class AskForm(forms.ModelForm):
 	class Meta:
 		model = Question
 		fields = ['title', 'text']
 
 	def clean(self):
-		if forms.is_spam(self.cleaned_data):
-			raise forms.ValidationError(u'This is spam', code='spam') 
+		if not is_ask(self.cleaned_data):
+			raise forms.ValidationError(u'This is not ask', code='12') 
 
 class AnswerForm(forms.ModelForm):
     class Meta:
@@ -16,5 +30,5 @@ class AnswerForm(forms.ModelForm):
     	fields = ['text', 'question']
 
     def clean(self):
-    	if forms.is_spam(self.cleaned_data):
-    		raise forms.ValidationError(u'This is spam', code='spam') 
+    	if not is_answer(self.cleaned_data):
+    		raise forms.ValidationError(u'This is not answer', code='12') 
