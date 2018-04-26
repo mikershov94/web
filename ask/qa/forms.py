84cba93437bdbe3cadabf9acd862976bb77ffb2a
mikershov94 <ershovme@gmail.com>
2018-04-26 11:12:37 +0800

    Fixed errors

--- ask/qa/forms.py
+++ ask/qa/forms.py
@@ -3,14 +3,14 @@ from qa.models import Question, Answer
 
 def is_ask(cleaned_data):
 	text = cleaned_data['text']
-	if text[len(text)] == '?':
+	if text[len(text)-1] == '?':
 		return True
 	else:
 		return False
 
 def is_answer(cleaned_data):
 	text = cleaned_data['text']
-	if text[len(text)] != '?':
+	if text[len(text)-1] != '?':
 		return True
 	else:
 		return False
