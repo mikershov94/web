f429d5d399347cbb3581621d8362ed8d6b5e60fe
mikershov94 <ershovme@gmail.com>
2018-04-28 11:55:28 +0800

    Fixed errors

--- ask/qa/forms.py
+++ ask/qa/forms.py
@@ -18,7 +18,8 @@ def is_answer(cleaned_data):
 class AskForm(forms.ModelForm):
 	class Meta:
 		model = Question
-		fields = ['title', 'text'] 
+		fields = ['title', 'text']
+
 
 class AnswerForm(forms.ModelForm):
     class Meta:
