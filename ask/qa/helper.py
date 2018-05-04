a429bd86164a71fbdb06348a4dbfcf51984a8bf8
mikershov94 <ershovme@gmail.com>
2018-05-04 11:29:51 +0800

    Edit middleware

---
+++ ask/qa/helper.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import User
+from django.contrib.models import Session
+import random
+
+def do_login(login, password):
+	try:
+		user = User.objects.get(login=login)
+	except User.DoesNotExist:
+		return None
+
+	if user.password != password:
+		return None
+
+	session = Session()
+	session.key = random.randint()
+	session.user = user
+	session.expires = datetime.now()+timedelta(days=5)
+	session.save()
+	return session.key
